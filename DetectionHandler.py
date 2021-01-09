a9cae84dc3255844182984f17c40521eb0aba9d0
TruongGlacio <buixuantruong.tb@gmail.com>
2021-01-09 08:12:48 +0700

    Add save image function

--- DetectionHandler.py
+++ DetectionHandler.py
@@ -1,3 +1,5 @@
+from operator import le
+import os
 from GlobalFileImport import GlobalImport
 import numpy as np
 import cv2
@@ -5,6 +7,8 @@ from FaceDetection import FaceDetection
 from PhoneDetection import PhoneDetection
 from VerhicleDetection import VerhicleDetection
 from Roadmarkerdetect import RoadmarkerDetection
+import datetime
+from time import gmtime, strftime
 class DetectHandler:
     def __init__(self):
         self.global_import = GlobalImport()
@@ -29,6 +33,10 @@ class DetectHandler:
             if frame is None:
                 return
             count += 1
+            EyeObject_arr = []
+            YawnMouthObject_arr=[]
+            looking_other_way_status_arr = []
+            dets = []
             if count%6 == 0:
 
                 frame,EyeObject_arr,YawnMouthObject_arr, looking_other_way_status_arr = self.faceDetection.Handler(frame)
@@ -40,11 +48,25 @@ class DetectHandler:
             #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             # Display the resulting frame
             cv2.imshow('frame',frame)
+            if len(EyeObject_arr) > 0 or len(YawnMouthObject_arr) > 0 or len(looking_other_way_status_arr) > 0 or len(dets) > 0:
+               self.SaveImage(frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         # When everything done, release the capture
         cap.release()
         cv2.destroyAllWindows()
+    def SaveImage(self,frame):
+        output_folder = 'Output/'
+        if not os.path.exists(output_folder):
+            os.mkdir(output_folder)       
+        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        file_name = str(current_time).replace('-','_')
+        file_name = file_name.replace(' ','_')
+        file_name = file_name.replace(':','_')
+        file_name = file_name.replace('.','_')
+        file_name = output_folder + file_name + '.png'
+        cv2.imwrite(file_name, frame) 
+
 
 
 def main():
